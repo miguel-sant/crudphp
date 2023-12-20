0294ce5f96c5b47c2353e649290e078aa3d9aa0f
Miguel Santos <84286333+miguel-sant@users.noreply.github.com>
2023-12-19 23:27:35 -0300

    Add files via upload

--- cadastrar_action.php
+++ cadastrar_action.php
@@ -10,40 +10,50 @@
     $rua = filter_input(INPUT_POST, 'rua');
     $cep = filter_input(INPUT_POST, 'cep');
     
-        
-        
-    $sql=$pdo->prepare("INSERT INTO endereco (bairro, rua, cep) values (:bairro, :rua, :cep)");
-    $sql->bindValue(':bairro', $bairro);
-    $sql->bindValue(':rua', $rua);
-    $sql->bindValue(':cep', $cep);
-    $sql->execute();
-
-    $sql = $pdo->query('SELECT MAX(id) as id from endereco');
-    $result = $sql->fetch(PDO::FETCH_ASSOC);
-    $idEndereco=$result['id'];
-
-    $sql=$pdo->prepare("INSERT INTO contatos (nome, email, telefone, setor, id_enderecofk) VALUES (:nome, :email, :telefone, :setor, :id_enderecofk)");
-    $sql->bindValue(':nome', $nome);
-    $sql->bindValue(':email', $email);
-    $sql->bindValue(':telefone', $telefone);
-    $sql->bindValue(':setor', $setor);
-    $sql->bindValue(':id_enderecofk', $idEndereco);
-    $sql->execute();      
-
+    try {
+        $pdo->beginTransaction();
     
-
-    $sql = $pdo->query('SELECT MAX(id) from contatos');
-    $idLista = $sql->fetch(PDO::FETCH_ASSOC);
-
-
-
-
-   $sql=$pdo->prepare("UPDATE contatos set id_enderecofk = :id_enderecofk where id = :id");
-    $sql->bindValue(':id_enderecofk', $idEndereco, PDO::PARAM_INT);
-    $sql->bindValue(':id', $idEndereco);
-     $sql->execute();
-
-     header('Location:index.php');
+       
+    
+        $sql=$pdo->prepare("INSERT INTO endereco (bairro, rua, cep) values (:bairro, :rua, :cep)");
+        $sql->bindValue(':bairro', $bairro);
+        $sql->bindValue(':rua', $rua);
+        $sql->bindValue(':cep', $cep);
+        $sql->execute();
+    
+        $sql = $pdo->query('SELECT MAX(id) as id from endereco');
+        $result = $sql->fetch(PDO::FETCH_ASSOC);
+        $idEndereco=$result['id'];
+    
+        $sql=$pdo->prepare("INSERT INTO contatos (nome, email, telefone, setor, id_enderecofk) VALUES (:nome, :email, :telefone, :setor, :id_enderecofk)");
+        $sql->bindValue(':nome', $nome);
+        $sql->bindValue(':email', $email);
+        $sql->bindValue(':telefone', $telefone);
+        $sql->bindValue(':setor', $setor);
+        $sql->bindValue(':id_enderecofk', $idEndereco);
+        $sql->execute();      
+    
+        
+    
+        $sql = $pdo->query('SELECT MAX(id) from contatos');
+        $idLista = $sql->fetch(PDO::FETCH_ASSOC);
+    
+    
+    
+    
+       $sql=$pdo->prepare("UPDATE contatos set id_enderecofk = :id_enderecofk where id = :id");
+        $sql->bindValue(':id_enderecofk', $idEndereco, PDO::PARAM_INT);
+        $sql->bindValue(':id', $idEndereco);
+         $sql->execute();
+    
+         header('Location:index.php');
+        $pdo->commit();
+        echo "Transação concluída com sucesso!";
+    } catch (Exception $e) {
+        $pdo->rollBack();
+        echo "Falha na transação: " . $e->getMessage();
+    }
+        
 
 
     
